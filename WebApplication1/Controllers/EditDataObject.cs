cd70bda25d9c66426e6887464ada6fbeb61653c1
SandroGitA <maktavish74@mail.ru>
2019-03-19 19:26:15 +0500

    Добавлен новый класс редактирования

---
+++ WebApplication1/Controllers/EditDataObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace WebApplication1.Controllers
+{
+    public class EditDataObject
+    {
+        public EditTask BuildData(string dataString)
+        {
+            EditTask responseJsonString = JsonConvert.DeserializeObject<EditTask>(dataString);
+
+            if ((string)responseJsonString.propName == "isComplete")
+                responseJsonString.value = ConvertBoolField.Convert(responseJsonString.value);
+            if ((string)responseJsonString.propName == "isPin")
+                responseJsonString.value = ConvertBoolField.Convert(responseJsonString.value);
+
+            return responseJsonString;
+        }
+    }
+}
