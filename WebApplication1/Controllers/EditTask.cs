446b6bd6691686b67d5a7cdcdb4c33272eaa6fdb
SandroGitA <maktavish74@mail.ru>
2019-03-19 19:26:45 +0500

    Класс редактирования

---
+++ WebApplication1/Controllers/EditTask.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class EditTask
+    {
+        public object id { get; set; }
+        public object propName { get; set; }
+        public object value { get; set; }
+    }
+}
