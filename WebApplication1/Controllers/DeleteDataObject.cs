bd741e23138ac60051e20b8765b4121503848907
SandroGitA <maktavish74@mail.ru>
2019-03-20 20:23:14 +0500

    Дрбавлена сборка класса DeleteTask

---
+++ WebApplication1/Controllers/DeleteDataObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace WebApplication1.Controllers
+{
+    public class DeleteDataObject
+    {
+        public DeleteTask BuildData(string dataString)
+        {
+            DeleteTask deleteTask = JsonConvert.DeserializeObject<DeleteTask>(dataString);
+
+            return deleteTask;
+        }
+    }
+}
