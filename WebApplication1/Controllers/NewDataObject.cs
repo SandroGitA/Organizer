d59d8bc749912f1eb95a0f7114eb407e8d2e6d41
SandroGitA <maktavish74@mail.ru>
2019-03-21 00:50:57 +0500

    Переименован файл из BuildData в NewDataObject

---
+++ WebApplication1/Controllers/NewDataObject.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace WebApplication1.Controllers
+{
+    public class BuildDataObject
+    {
+        public newTask BuildData(string dataString)
+        {
+            newTask responseJsonString = JsonConvert.DeserializeObject<newTask>(dataString);
+
+            responseJsonString.isPin = ConvertBoolField.Convert(responseJsonString.isPin);
+            responseJsonString.isComplete = ConvertBoolField.Convert(responseJsonString.isComplete);
+            responseJsonString.dateBind = ConvertDataField.Convert(responseJsonString.dateBind);
+            responseJsonString.dateCreate = ConvertDataField.Convert(responseJsonString.dateCreate);
+
+            return responseJsonString;
+        }
+    }
+}
