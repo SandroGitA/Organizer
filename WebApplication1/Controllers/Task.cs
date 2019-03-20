c9b2f198880f71bac87473279b97b9f09e87dac8
SandroGitA <maktavish74@mail.ru>
2019-03-21 00:51:34 +0500

    переименован файл Data в Task

---
+++ WebApplication1/Controllers/Task.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WebApplication1.Controllers
+{
+    public class newTask
+    {      
+        public object dateBind { get; set; }
+        public object dateCreate { get; set; }
+        public object descr { get; set; }
+        public object id { get; set; }
+        public object isComplete { get; set; }
+        public object isPin { get; set; }
+        public object title { get; set; }                   
+    }
+}
